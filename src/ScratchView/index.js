15d72ca2096168dd1a7c1fe1e17fb9ba45888046
Priyansh Gupta <priyanshgupta1161@gmail.com>
2019-12-20 13:12:43 +0530

    Update index.js

--- src/ScratchView/index.js
+++ src/ScratchView/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Animated, TouchableWithoutFeedback } from 'react-native';
+import { StyleSheet, Animated, TouchableWithoutFeedback, Easing } from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
